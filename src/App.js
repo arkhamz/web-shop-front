89f40eba53cf7914ce624ad674ec77061a9d74cb
Jona <mr-jona@hotmail.com>
2022-07-20 13:48:17 +0200

    insert cart state and cart event handler

--- src/App.js
+++ src/App.js
@@ -3,15 +3,28 @@ import { Navbar } from "./components";
 import { Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Details from "./pages/Details";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  //this gets passed down and takes a product object. updates cart state when cart btns are clicked
+  const cartUpdater = (productObject) => {
+    //make a copy and add the newly clicked product to it
+    const newCart = [...cart, productObject];
+    setCart(newCart);
+  };
+
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <Routes>
           <Route path="/" element={<Product />} />
-          <Route path="/details/:id" element={<Details />} />
+          <Route
+            path="/details/:id"
+            element={<Details cartUpdater={cartUpdater} />}
+          />
           <Route />
         </Routes>
       </div>
