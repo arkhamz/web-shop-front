b58b181721cbc15eee71bc56643d7ef8fd8baae3
Jona <mr-jona@hotmail.com>
2022-07-20 16:45:08 +0200

    style cart list up until inserting actual products

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,6 +2,17 @@ import { Link } from "react-router-dom";
 import "./Cart.css";
 
 const Cart = ({ cartState }) => {
+  //if cartState is empty put a placeholder instead
+  const renderProducts =
+    cartState.length < 1
+      ? "nothing in cart yet!"
+      : cartState.map((product) => {
+          return (
+            <div key={product.id} className="cartListItem">
+              {product.title}
+            </div>
+          );
+        });
   return (
     <div className="cartPageContainer">
       <div className="links">
@@ -21,13 +32,37 @@ const Cart = ({ cartState }) => {
             <h3 id="quantityH3" className="cartH3">
               Quantity
             </h3>
-            <h3 id="totalH3" className="cartH3">
+            <h3 id="productTotalH3" className="cartH3">
               Total
             </h3>
           </div>
-          <div className="cartProductList">list</div>
+          <div className="cartProductList">{renderProducts}</div>
+          <div className="cartListBtns">
+            <button>Update Cart</button>
+            <button>Clear Cart</button>
+          </div>
         </div>
-        <div className="cartTotals">carttotals</div>
+        <div className="cartTotalsDiv">
+          <div className="columNames">
+            <h3 id="totalsH3" className="cartH3">
+              Cart Totals
+            </h3>
+          </div>
+          <div className="totalsProceedCard">
+            <div id="subTotals">
+              <h4>Subtotals:</h4> <h4>eurosss</h4>
+            </div>
+            <div id="finalTotals">
+              <h4>Totals:</h4> <h4>eurosss</h4>
+            </div>
+            <div id="shipText">
+              <p>🟢 Shipping & taxes calculated at checkout</p>
+            </div>
+            <div id="proceedButtonDiv">
+              <button>Proceed To Checkout</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
