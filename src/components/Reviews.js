f833c1c2b37a808e162d2d579c239110e9d4d5c2
Jona <mr-jona@hotmail.com>
2022-07-22 12:55:02 +0200

    add styling to review text and date

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./Reviews.css";
 
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+
 const Reviews = () => {
   const [reviews, setReviews] = useState("");
   const { id } = useParams();
@@ -20,14 +22,34 @@ const Reviews = () => {
     getReviews();
   }, [id]);
 
+  const generateStars = (rating) => {
+    const stars = [];
+    for (let i = 0; i < 5; i++) {
+      if (i < rating) {
+        stars.push(<AiFillStar key={i} />);
+      } else {
+        stars.push(<AiOutlineStar key={i} />);
+      }
+    }
+    return stars;
+  };
+
+  const dateFormatter = (dateDromDB) => {
+    const newDate = new Date(dateDromDB);
+    const stringDate = newDate.toString();
+    const arrayDate = stringDate.split(" ").splice(0, 5).join(" ");
+    return arrayDate;
+  };
+
   const renderReviews = !reviews
     ? "no reviews yet!"
     : reviews.map((review) => {
         return (
           <div key={review.id} className="reviewInstance">
             <h3>{review.ReviewerName}</h3>
-            <p className="reviewRating">rating: {review.rating}</p>
+            <p className="reviewRating">{generateStars(review.rating)}</p>
             <p className="reviewText">{review.text}</p>
+            <p className="reviewTime">-{dateFormatter(review.createdAt)}</p>
           </div>
         );
       });
